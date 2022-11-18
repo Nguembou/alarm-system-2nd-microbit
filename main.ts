487ffff7ddcc20af6db4f4867fc72fa9af61dc84
Nguembou <113910948+Nguembou@users.noreply.github.com>
2022-11-18 08:33:40 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,35 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 2) {
-        basic.showLeds(`
-            # . . . #
-            . # . # .
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
-        music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Forever)
+    	
     }
 })
 input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(4)
+    Entry = "" + Entry + "A"
 })
+input.onButtonPressed(Button.AB, function () {
+    if (Entry == Password) {
+        radio.sendNumber(4)
+        pins.servoWritePin(AnalogPin.P0, 0)
+        basic.showIcon(IconNames.Yes)
+        music.stopMelody(MelodyStopOptions.All)
+    } else {
+        basic.showIcon(IconNames.No)
+        Entry = ""
+        basic.showIcon(IconNames.Sad)
+        music.playTone(175, music.beat(BeatFraction.Whole))
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    Entry = "" + Entry + "B"
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Sad)
+    music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Forever)
+})
+let Password = ""
+let Entry = ""
 basic.showIcon(IconNames.Yes)
+Entry = ""
+Password = "BABAA"
+pins.servoWritePin(AnalogPin.P0, 0)
 radio.setGroup(77)
-basic.forever(function () {
-	
-})
